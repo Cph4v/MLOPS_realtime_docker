22524d371944cf0c9117c760d8b112c64fb1ba36
Cph4v <64288747+Cph4v@users.noreply.github.com>
2024-10-31 06:30:07 -0400

    Delete realtime_data directory

--- realtime_data/configs/discord_settings.py
+++
@@ -1,7 +0,0 @@
-
-discord_url = {
-  "ch-1":"https://discord.com/api/webhooks/1248709666943402044/Rq3tJHhRBUCx8It1S5NbzZJMxUPx5-Z5BhoII0f6rmkjrzUK9p8giLb002j8Nq5Ny6P4",
-  "ch-2":"https://discord.com/api/webhooks/1249432877859344545/dTTbu6z0jpkdKifSFgaJX3T0WTv5BI6QI9-2nCohs5h-YEN7H9yrXSDshEwK9TbDce0X",
-
-
-}
